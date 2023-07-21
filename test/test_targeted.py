30d90978db8933e58181362f438f5337ffc0cebd
simonsben <22123881+simonsben@users.noreply.github.com>
2023-07-21 14:46:59 -0400

    test: named edge target

--- test/test_targeted.py
+++ test/test_targeted.py
@@ -1,12 +1,19 @@
 from typing import Callable
 
-from flow_control.flow import flow_edge
+from pytest import mark
+
+from flow_control.flow import custom_flow_edge
 from flow_control.targeted import to_edge_target
 from test.test_flow import DEFAULT_RETURN
 
+EDGE_NAME = "test_edge"
 
-def test_targeted_jump() -> None:
-    @flow_edge
+
+@mark.parametrize("target_by_name", (True, False), ids=("name_target", "function_target"))
+def test_targeted_jump(target_by_name: bool) -> None:
+    """Ensure we can properly jump to targeted edges."""
+
+    @custom_flow_edge(name=EDGE_NAME)
     def function(inner_function: Callable[[], None]) -> None:
         """Function with flow edge."""
 
@@ -14,6 +21,7 @@ def test_targeted_jump() -> None:
 
     def _inner_function() -> None:
         """Inner function requesting the jump."""
-        to_edge_target(function, DEFAULT_RETURN)
+        target = EDGE_NAME if target_by_name else function
+        to_edge_target(target, DEFAULT_RETURN)
 
     assert function(lambda: _inner_function()) == DEFAULT_RETURN
